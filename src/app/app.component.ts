df33bd1542375261a7216c811ed75163666fad3a
Marion Communal <marion.communal@hotmail.fr>
2020-09-14 18:25:24 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import * as firebase from 'firebase'
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -20,6 +22,19 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
+      let firebaseConfig = {
+        apiKey: "AIzaSyBtj8cAicjOhuPn2wdUhCXsNjn2mIRiXOg",
+        authDomain: "chat-bf90b.firebaseapp.com",
+        databaseURL: "https://chat-bf90b.firebaseio.com",
+        projectId: "chat-bf90b",
+        storageBucket: "chat-bf90b.appspot.com",
+        messagingSenderId: "373752974801",
+        appId: "1:373752974801:web:ed8f591ad3052cbb2b64f0",
+        measurementId: "G-QGVLL3ER82"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+import { Facebook } from '@ionic-native/facebook/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  providerFb: firebase.auth.FacebookAuthProvider;
 
-  constructor() {}
+  constructor(public afDB: AngularFireDatabase,
+    public afAuth: AngularFireAuth,
+    private fb: Facebook,
+    public platform: Platform) {
+      this.providerFb = new firebase.auth.FacebookAuthProvider()
+    }
+
+    facebookLogin() {
+      if (this.platform.is('cordova')) {
+        console.log('PLateforme cordova');
+    
+      } else {
+        console.log('PLateforme Web');
+
+      }
+  }
 
 }
